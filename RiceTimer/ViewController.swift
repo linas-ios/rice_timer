757f1b4aba8e7c05ec5adca8a6e350069e83a072
linas-ios <linasn404@gmail.com>
2023-05-25 19:42:23 +0200

    create UI

--- RiceTimer/ViewController.swift
+++ RiceTimer/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var startButton: UIButton!
+  @IBOutlet weak var segmentControl: UISegmentedControl!
+  @IBOutlet weak var riceTypeImage: UIImageView!
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    configureSegmentControl()
+    configureStartButton()
+
+
+  }
+
+  @IBAction func segmentControlValueChanged(_ sender: UISegmentedControl) {
+
+    switch sender.selectedSegmentIndex {
+    case 0:
+      riceTypeImage.image = UIImage(named: "white")
+    case 1:
+      riceTypeImage.image = UIImage(named: "brown")
+    case 2:
+      riceTypeImage.image = UIImage(named: "wild")
+    default:
+      print("Error")
+    }
+  }
+
+
+  func configureSegmentControl() {
+    riceTypeImage.image = UIImage(named: "white")
+    let notSelectedColor = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    let selectedColor = [NSAttributedString.Key.foregroundColor: UIColor.black]
+    segmentControl.setTitleTextAttributes(notSelectedColor, for: .normal)
+    segmentControl.setTitleTextAttributes(selectedColor, for: .selected)
+
+
+  }
+
+  func configureStartButton() {
+    startButton.layer.cornerRadius = 15
+    startButton.clipsToBounds = true
   }
 
 
